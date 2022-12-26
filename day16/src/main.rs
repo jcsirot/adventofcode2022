fd30841eb5ea8756deeb56e35fba4d9979fac269
Jean-Christophe Sirot <jcsirot@gmail.com>
2022-12-26 11:19:07 +0100

    Add Day 16 part 2
    Signed-off-by: Jean-Christophe Sirot <jcsirot@gmail.com>

--- day16/src/main.rs
+++ day16/src/main.rs
@@ -1,22 +1,28 @@
-use std::fs;
-use std::env;
 use regex::Regex;
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+use std::env;
+use std::fs;
 
 fn main() {
     let mut args = env::args();
     args.next();
 
     let filename = match args.next() {
-        Some(arg) => { arg }
-        None => { panic!("Filename is missing"); }
+        Some(arg) => arg,
+        None => {
+            panic!("Filename is missing");
+        }
     };
 
     let input = match fs::read_to_string(filename) {
-        Err(err) => { panic!("{}", err); }
-        Ok(v) => { v }
+        Err(err) => {
+            panic!("{}", err);
+        }
+        Ok(v) => v,
     };
-    
+
     let part1 = solve_part_1(&input);
     println!("part 1: {}", part1);
     let part2 = solve_part_2(&input);
@@ -26,26 +32,28 @@ fn main() {
 #[derive(Debug, PartialEq, Eq)]
 struct Valve {
     id: String,
-    rate: u32,
+    rate: i32,
     links: Vec<String>,
 }
 
 #[derive(Debug)]
 struct Room {
-    id: String,
-    rate: u32,
-    paths: HashMap<String, u32>,
+    rate: i32,
+    paths: HashMap<String, i32>,
 }
 
 fn parse(input: &str) -> HashMap<String, Valve> {
-    let re = Regex::new(r"^Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.+)$").unwrap();
+    let re = Regex::new(
+        r"^Valve ([A-Z][A-Z]) has flow rate=(\d+); tunnel[s]? lead[s]? to valve[s]? (.+)$",
+    )
+    .unwrap();
     let mut valves = HashMap::<String, Valve>::new();
     for line in input.lines() {
         let captures = re.captures(line).unwrap();
         let id = captures[1].to_string();
-        let rate = captures[2].parse::<u32>().unwrap();
+        let rate = captures[2].parse::<i32>().unwrap();
         let links: Vec<String> = captures[3].split(", ").map(|s| s.to_string()).collect();
-        let valve = Valve{
+        let valve = Valve {
             id: id,
             rate: rate,
             links: links,
@@ -55,64 +63,75 @@ fn parse(input: &str) -> HashMap<String, Valve> {
     valves
 }
 
-fn compute_path(start: &str, end: &str, valves: &HashMap<String, Valve>) -> u32 {
-    let mut steps = 1;
-    let mut tunnels = Vec::new();
-    for r in &valves[start].links {
-        tunnels.push(r);
-    }
+fn compute_path(start: &str, end: &str, valves: &HashMap<String, Valve>) -> i32 {
+    let mut stack = VecDeque::new();
+    stack.push_back((start, 0));
+    let mut visited = HashSet::new();
     loop {
-        let mut next_rooms = Vec::<&String>::new();
-        for room in tunnels {
-            if room == end {
-                return steps;
-            }
-            for next_room in &valves[room].links {
-                next_rooms.push(next_room);
+        if stack.is_empty() {
+            break;
+        }
+        let (current, step) = stack.pop_front().unwrap();
+        if visited.contains(current) {
+            continue;
+        }
+        if current == end {
+            return step;
+        } else {
+            for next_room in &valves[current].links {
+                stack.push_back((next_room.as_str(), step + 1));
             }
         }
-        tunnels = next_rooms;
-        steps += 1;
+        visited.insert(current);
     }
+    0
 }
 
-fn walk<'a> (current: &'a str, visited: &Vec<&'a str>, remaining_time: i32, rooms: &HashMap<String, Room>) -> u32 {
-    //dbg!();
+fn walk<'a>(
+    current: &'a str,
+    remaining_rooms: &HashSet<&'a str>,
+    remaining_time: i32,
+    rooms: &HashMap<String, Room>,
+) -> i32 {
     //dbg!(current, visited, remaining_time-1);
-    //dbg!();
     if remaining_time <= 0 {
         return 0;
     }
-    
-    let mut new_visited = visited.clone();
-    new_visited.push(current);
 
-    //let new_flow = flow + (remaining_time - 1) * rooms[room].rate;
+    let mut new_remaining_rooms = remaining_rooms.clone();
+    new_remaining_rooms.remove(current);
+
+    //let new_flow = flow + remaining_time * rooms[room].rate;
 
     let mut best_flow = 0;
-    let flow = (remaining_time - 1) as u32 * rooms[current].rate;
-    for (candidate, len) in &rooms[current].paths {
-        if visited.contains(&candidate.as_str()) {
+    let rate = rooms[current].rate;
+    let flow = remaining_time * rate;
+    //dbg!(current, flow, remaining_time);
+    for (room, len) in &rooms[current].paths {
+        if !new_remaining_rooms.contains(&room.as_str()) {
             continue;
         }
-        let candidate_flow = flow + walk(&candidate, &new_visited, remaining_time-(len+1) as i32, rooms);
-        best_flow = u32::max(best_flow, candidate_flow);
+        let remaining_flow = walk(&room, &new_remaining_rooms, remaining_time - len - 1, rooms);
+        best_flow = best_flow.max(remaining_flow);
     }
-    return best_flow;
+    return flow + best_flow;
 }
 
-fn solve_part_1(input: &str) -> u32 {
+fn make_room_graph(input: &str) -> HashMap<String, Room> {
     let valves = parse(input);
-    let mut v: Vec<String> = valves.iter().filter(|&(_, v)| v.rate > 0 || v.id == "AA").map(|(k, _)| k.clone()).collect::<Vec<String>>();
-    v.sort();
+    let mut working_valves: Vec<String> = valves
+        .iter()
+        .filter(|&(_, v)| v.rate > 0 || v.id == "AA")
+        .map(|(k, _)| k.clone())
+        .collect::<Vec<String>>();
+    working_valves.sort();
     let mut rooms = HashMap::new();
-    for from in &v {
-        let mut room = Room { 
-            id: from.clone(),
+    for from in &working_valves {
+        let mut room = Room {
             rate: valves[from].rate,
             paths: HashMap::new(),
         };
-        for to in &v {
+        for to in &working_valves {
             if from == to {
                 continue;
             }
@@ -121,9 +140,52 @@ fn solve_part_1(input: &str) -> u32 {
         }
         rooms.insert(from.clone(), room);
     }
-    return walk("AA", &vec![], 31, &rooms);
+    // dbg!(&rooms);
+    rooms
 }
 
-fn solve_part_2(input: &str) -> u32 {
-    0
+fn subsets<T>(s: &[T]) -> Vec<Vec<T>>
+where
+    T: Clone,
+{
+    (0..2usize.pow(s.len() as u32))
+        .map(|i| {
+            s.iter()
+                .enumerate()
+                .filter(|&(t, _)| (i >> t) % 2 == 1)
+                .map(|(_, element)| element.clone())
+                .collect()
+        })
+        .collect()
+}
+
+fn solve_part_1(input: &str) -> i32 {
+    let rooms = make_room_graph(input);
+    let remaining_rooms = rooms.keys().map(|s| s.as_str()).collect::<HashSet<&str>>();
+    walk("AA", &remaining_rooms, 30, &rooms)
+}
+
+fn solve_part_2(input: &str) -> i32 {
+    let rooms = make_room_graph(input);
+    let all_rooms = subsets(&rooms.keys().map(|s| s.as_str()).collect::<Vec<&str>>());
+    // dbg!(all_rooms.len());
+    let mut best_stream = 0;
+    for set in &all_rooms {
+        let remaining_rooms = set.iter().cloned().collect::<HashSet<&str>>();
+        let elephant_rooms = rooms
+            .keys()
+            .filter_map(|s| {
+                if set.contains(&s.as_str()) {
+                    None
+                } else {
+                    Some(s.as_str())
+                }
+            })
+            .collect::<HashSet<&str>>();
+        //dbg!(&remaining_rooms, &elephant_rooms);
+        best_stream = best_stream.max(
+            walk("AA", &remaining_rooms, 26, &rooms) + walk("AA", &elephant_rooms, 26, &rooms),
+        );
+    }
+    best_stream
 }
